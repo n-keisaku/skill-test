c1562626049881dd3ef7269e13acf71fc985a32c
n-keisaku <0nakamura0k@gmail.com>
2021-04-03 05:50:58 +0900

    不要なファイルを作らないように設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module SkillTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
